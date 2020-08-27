e4be7076793462fbdc2c28a2192348bb639893be
KENIL DOSHI (thesmartyking ) <doshikenil623@gmail.com>
2020-08-27 22:20:34 +0530

    Initial Commit

--- ToasterLibrary/src/main/java/com/kenildoshi/toasterlibrary/ToastMsg.kt
+++ ToasterLibrary/src/main/java/com/kenildoshi/toasterlibrary/ToastMsg.kt
@@ -8,6 +8,8 @@ import android.widget.Toast
 
 class ToastMsg {
 
+    fun ToastMsg() {}
+
     fun st(c: Context, message: String) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
